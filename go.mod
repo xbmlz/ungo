eeb6490cf7c0ff5c08ab45564841ae864b97065a
xbmlz <997909544@qq.com>
2024-07-11 18:01:09 +0800

    feat: rm shutdown

--- go.mod
+++ go.mod
@@ -5,12 +5,10 @@ go 1.20
 require (
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/spf13/viper v1.19.0
-	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
@@ -19,7 +17,6 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- unhttp/http.go
+++ unhttp/http.go
@@ -4,8 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/xbmlz/ungo/unlog"
 )
 
 type Config struct {
@@ -27,20 +33,33 @@ func NewServer(handler http.Handler, config Config) *Server {
 	return server
 }
 
-func (s *Server) Start() (err error) {
-	err = s.srv.ListenAndServe()
-	if !errors.Is(err, http.ErrServerClosed) {
-		return err
-	}
-	return nil
-}
+func (s *Server) Run() (err error) {
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := s.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-func (s *Server) Shutdown() (err error) {
-	return s.srv.Shutdown(context.Background())
-}
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 
-func (s *Server) ShutdownWithTimeout(timeout time.Duration) (err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	unlog.Infof("Shutting down server...")
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	return s.srv.Shutdown(ctx)
+	if err := s.srv.Shutdown(ctx); err != nil {
+		return errors.New("Server forced to shutdown: " + err.Error())
+	}
+
+	unlog.Infof("Server exiting")
+	return nil
 }
