81c4cf12cd3ed55676ba66fce13af34b25d4276f
xbmlz <997909544@qq.com>
2024-07-15 17:01:02 +0800

    chore: format log dir

--- log/zap.go
+++ log/zap.go
@@ -120,3 +120,59 @@ func customCallerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayE
 func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
 }
+
+// Debug log
+func (z *zapLogger) Debug(v ...any) {
+	if z.conf.Level <= LevelDebug {
+		z.slog.Debug(v...)
+	}
+}
+
+// Debugf log
+func (z *zapLogger) Debugf(format string, v ...any) {
+	if z.conf.Level <= LevelDebug {
+		z.slog.Debugf(format, v...)
+	}
+}
+
+// Info log
+func (z *zapLogger) Info(v ...any) {
+	if z.conf.Level <= LevelInfo {
+		z.slog.Info(v...)
+	}
+}
+
+// Infof log
+func (z *zapLogger) Infof(format string, v ...any) {
+	if z.conf.Level <= LevelInfo {
+		z.slog.Infof(format, v...)
+	}
+}
+
+// Warn log
+func (z *zapLogger) Warn(v ...any) {
+	if z.conf.Level <= LevelWarn {
+		z.slog.Warn(v...)
+	}
+}
+
+// Warnf log
+func (z *zapLogger) Warnf(format string, v ...any) {
+	if z.conf.Level <= LevelWarn {
+		z.slog.Warnf(format, v...)
+	}
+}
+
+// Error log
+func (z *zapLogger) Error(v ...any) {
+	if z.conf.Level <= LevelError {
+		z.slog.Error(v...)
+	}
+}
+
+// Errorf log
+func (z *zapLogger) Errorf(format string, v ...any) {
+	if z.conf.Level <= LevelError {
+		z.slog.Errorf(format, v...)
+	}
+}
